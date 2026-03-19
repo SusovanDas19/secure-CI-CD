b4f88228e1d94c7595bf04302adf958580847616
root <root@ip-172-31-33-102.us-east-2.compute.internal>
2026-03-19 14:34:19 +0000

    added detectable secret

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Hello Secure CI/CD");
 const password = "123456";
 const AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE";
 console.log("Testing secret detection");
+const github_token = "ghp_1234567890abcdefghijklmnopqrstuvwxyz";
